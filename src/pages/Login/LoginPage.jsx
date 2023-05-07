41233e95c78f2462ab99b61c446c83a6f1d7fbe5
thanhtra <19522370@gm.uit.edu.vn>
2023-05-07 13:38:37 +0700

    fix

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="leftContainer">
+          <img
+            src="https://www.nhavanhoasinhvien.vn/wp-content/uploads/bfi_thumb/slide-1-o7vxuvddx981jmy4pn1d4zv3aisbzkq2n9whu71u7m.jpg"
+            alt=""
+          />
+        </div>
+        <div className="rightContainer"></div>
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
